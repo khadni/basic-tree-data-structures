3b6475da0e3a0ca4f49600cf0f9a603d4eec86da
Karim <98668332+khadni@users.noreply.github.com>
2022-12-07 13:30:59 +0100

    branching

--- trie-nodes/trie.js
+++ trie-nodes/trie.js
@@ -7,14 +7,16 @@ const TrieNode = require("./TrieNode");
 
 class Trie {
   constructor() {
-    this.root = new TrieNode(null); // null key to indicate the top parent in the list
+    this.root = new TrieNode(null);
   }
-
   insert(word) {
     let node = this.root;
 
     for (let i = 0; i < word.length; i++) {
-      node.children[word[i]] = new TrieNode(word[i]);
+      if (!node.children[word[i]]) {
+        node.children[word[i]] = new TrieNode(word[i]);
+      }
+
       node = node.children[word[i]];
 
       if (i == word.length - 1) {
